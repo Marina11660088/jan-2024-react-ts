5a427fc10122ff98ddb47857f690bfaebc25e539
Marina11660088 <marinka.um@gmail.com>
2024-05-02 18:44:12 +0300

    homework1

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import './App.css';
 import Character from "./components/character/Character";
 import Characters from "./components/characters/Characters";
 import Recipes from "./components/recipes/Recipes";
+import Products from "./components/products/Products";
 
 const App = () => {
   return (
       <>
-<Recipes/>
+<Products/>
 
       </>
   )
---
+++ src/components/product/Product.tsx
@@ -0,0 +1,33 @@
+import React, {FC} from 'react';
+
+
+export interface IProductProps {
+    id: number,
+    title: string,
+    description: string,
+    price: number,
+    discountPercentage: number,
+    rating: number,
+    stock: number,
+    brand: string,
+    category:string,
+    thumbnail: string,
+    images: string[]
+}
+export type IProductTypeProps = IProductProps & { children?: React.ReactNode};
+
+const Product : FC <IProductTypeProps> = ({id,title,description,price,discountPercentage,rating,stock,brand,category,thumbnail,images}) => {
+    return (
+        <div>
+<h2>{id}. {title}. {description}. {price}. {discountPercentage}. {rating}. {stock}. {brand}. {category}. {thumbnail}</h2>
+        <div>
+            {
+                images.map((image,index)=><img src={image} alt={title}/>)
+            }
+        </div>
+
+        </div>
+    );
+};
+
+export default Product;
